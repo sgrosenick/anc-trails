58c97d5f72e0bbaa11c6b2091610e08de48aaffb
Samuel Grosenick <srgrosenick@gmail.com>
2020-10-17 09:16:11 -0800

    Added header component

--- anc-trails/src/App.js
+++ anc-trails/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Map from './Map/Map';
+import Header from './Header/Header'
 import Controls from './Controls/Control';
  
 function App() {
   return (
     <div className="map-container">
+      <Header />
       <Controls />
       <Map />
     </div>
---
+++ anc-trails/src/Header/Header.js
@@ -0,0 +1,13 @@
+    import React, { Component } from 'react';
+    import { NavLink } from 'react-router-dom';
+    
+    export default class Header extends Component {
+        render() {
+            return (
+                <nav id='header'>
+                    <div>Anchorage Trails</div>
+                </nav>
+            )
+        }
+    }
+    
